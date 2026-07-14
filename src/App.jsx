3acf90f2fad58e8f104c7404c6cd11f5f776556f
Isaiah Rodriguez <irod196@wgu.edu>
2026-07-14 16:53:34 -0500

    Created state and input

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,41 @@ import './App.css'
 
 function App() {
   // Add your code here
-    
+    const [balance, setBalance] = useState("")
+    const [rate, setRate] = useState("")
+    const [term, setTerm] = useState(0)
+    const [output, setOutput] = useState("")
+
   return (
     <>
-      {/* Add your code here */}
+      <h1>Mortgage Calculator</h1>
+
+      <input 
+      data-testid="balance"
+      type="number"
+      value={balance}
+      onChange={(e) => setBalance(e.target.value)}
+      />
+
+      <input
+      data-testid="rate"
+      type="number"
+      step="0.01"
+      value={rate}
+      onChange={(e) => setRate(e.target.value)}
+      />
+
+      <select 
+      data-testid="term"
+      value={term}
+      onChange={(e) => setTerm(e.target.value)}> 
+      <option value="15">15</option>
+      <option value="30">30</option>
+      </select>
+
+      <button data-testid="submit">Calculate</button>
+
+      <div id="output" data-testid="output">{output}</div>
 
     </>
   )
