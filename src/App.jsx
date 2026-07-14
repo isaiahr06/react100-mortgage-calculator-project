c63e3c64aeb13696fe8b2f1aa5d885caef5ac725
Isaiah Rodriguez <irod196@wgu.edu>
2026-07-14 17:57:56 -0500

    Created App and calculate

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import {useState} from 'react';
 import './App.css'
 
 function App() {
   // Add your code here
     const [balance, setBalance] = useState("")
     const [rate, setRate] = useState("")
-    const [term, setTerm] = useState(0)
+    const [term, setTerm] = useState("15")
     const [output, setOutput] = useState("")
+
+    function calculate(balance,rate,term){
+      const principal = Number(balance);
+      const monthlyInterestRate = Number(rate / 100 / 12);
+      const numberOfPayments = Number(term * 12);
+      const monthlyPayment = principal * (monthlyInterestRate * (1 + monthlyInterestRate)** numberOfPayments) / ((1 + monthlyInterestRate)**numberOfPayments - 1)
+
+      setOutput(`$${monthlyPayment.toFixed(2)} is your payment`)
+    }
 
   return (
     <>
       <h1>Mortgage Calculator</h1>
 
+      <label>Loan Balance</label>
       <input 
       data-testid="balance"
       type="number"
@@ -18,6 +29,7 @@ function App() {
       onChange={(e) => setBalance(e.target.value)}
       />
 
+      <label>Interest Rate</label>
       <input
       data-testid="rate"
       type="number"
@@ -26,6 +38,7 @@ function App() {
       onChange={(e) => setRate(e.target.value)}
       />
 
+      <label>Loan Term</label>
       <select 
       data-testid="term"
       value={term}
@@ -34,7 +47,7 @@ function App() {
       <option value="30">30</option>
       </select>
 
-      <button data-testid="submit">Calculate</button>
+      <button data-testid="submit" onClick={() => calculate(balance, rate, term)}>Calculate</button>
 
       <div id="output" data-testid="output">{output}</div>
 
